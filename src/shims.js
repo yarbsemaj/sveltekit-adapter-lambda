373c91d34b150a0aa21b5657ab36164da2820185
James Bray <james.bray@jdplc.com>
2022-02-28 22:56:43 +0000

    Move to node-fetch for fetch

--- src/shims.js
+++ src/shims.js
@@ -1,1 +1,3 @@
-export { fetch, Response, Request, Headers } from '@sveltejs/kit/install-fetch';
+import fetch from 'node-fetch';
+import {Response, Request, Headers } from 'node-fetch';
+export {fetch, Response, Request, Headers }
